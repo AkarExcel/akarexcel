1ebd381d37f22facce9ba785170b8dd3028f8b91
Akar Excel <therealakarexcel@gmail.com>
2022-10-31 15:49:55 +0100

    add navbar

--- layout/Navbar.js
+++ layout/Navbar.js
@@ -1,37 +1,148 @@
 import Image from 'next/image'
+import { useState } from 'react'
 import Link from 'next/link'
+import Script from 'next/script'
 import React from 'react'
+import $ from "jquery"
+
+
+
+if(typeof window !== "undefined"){
+    // Menu Navigation
+    
+
+//     var OnePageNav = function () {
+//       var navToggler = $('.nilsbrown-js-nav-toggle');
+//      $(".smoothscroll[href^='#'], #nilsbrown-navbar ul li a[href^='#']").on('click', function (e) {
+//          e.preventDefault();
+//          var hash = this.hash;
+//          $('html, body').animate({
+//              scrollTop: $(hash).offset().top
+//          }, 700, 'easeInOutExpo', function () {
+//              window.location.hash = hash;
+//          });
+//      });
+//      $("#nilsbrown-navbar ul li a[href^='#']").on('click', function (e) {
+//          if (navToggler.is(':visible')) {
+//              navToggler.click();
+//          }
+//      });
+//  };
+//  OnePageNav();
+ $(window).scroll(function () {
+     var $this = $(this)
+         , st = $this.scrollTop()
+         , navbar = $('.nilsbrown-header')
+         , logo = $(".nilsbrown-header .logo> img");
+     if (st > 150) {
+         if (!navbar.hasClass('scrolled')) {
+             navbar.addClass('scrolled');
+             logo.attr('src', 'images/logo-dark.png');
+         }
+     }
+     if (st < 150) {
+         if (navbar.hasClass('scrolled')) {
+             navbar.removeClass('scrolled sleep');
+             logo.attr('src', 'images/logo-dark.png');
+         }
+     }
+     if (st > 350) {
+         if (!navbar.hasClass('awake')) {
+             navbar.addClass('awake');
+         }
+     }
+     if (st < 350) {
+         if (navbar.hasClass('awake')) {
+             navbar.removeClass('awake');
+             navbar.addClass('sleep');
+         }
+     }
+ });
+//  $('.nilsbrown-js-nav-toggle').on('click', function (e) {
+//      var $this = $(this);
+//      e.preventDefault();
+//      if ($('body').hasClass('menu-open')) {
+//          $this.removeClass('active');
+//          $('.nilsbrown-menu .nilsbrown-menu-inner > ul > li').each(function (i) {
+//              var that = $(this);
+//              setTimeout(function () {
+//                  that.removeClass('is-show');
+//              }, i * 100);
+//          });
+//          setTimeout(function () {
+//              $('.nilsbrown-menu').removeClass('nilsbrown-menu-show');
+//          }, 800);
+//          $('body').removeClass('menu-open');
+//      }
+//      else {
+//          $('.nilsbrown-menu').addClass('nilsbrown-menu-show');
+       
+//          $this.addClass('active');
+//          $('body').addClass('menu-open');
+//          setTimeout(function () {
+//              $('.nilsbrown-menu .nilsbrown-menu-inner > ul > li').each(function (i) {
+//                  var that = $(this);
+//                  setTimeout(function () {
+//                      that.addClass('is-show');
+//                  }, i * 100);
+//              });
+//          }, 500);
+//      }
+//  });
+//  $('.nilsbrown-menu .dropdown').hover(function () {
+//      var $this = $(this);
+//      $this.addClass('show');
+//      $this.find('> a').attr('aria-expanded', true);
+//      $this.find('.dropdown-menu').addClass('show');
+//  }, function () {
+//      var $this = $(this);
+//      $this.removeClass('show');
+//      $this.find('> a').attr('aria-expanded', false);
+//      $this.find('.dropdown-menu').removeClass('show');
+//  });
+
+
+   
+}
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const handleShow = () => {
+    setShow(!show)
+    if(typeof window !== "undefined"){
+    document.querySelector('body').classList.toggle("menu-open")
+    }
+  }
   return (
 <>
   {/* Menu */}
-  <nav className="nilsbrown-menu" id="nilsbrown-navbar-spy">
+  {/* <nav className={`nilsbrown-menu ${show?"nilsbrown-menu":""}`}id="nilsbrown-navbar-spy"> */}
+  <nav className={`nilsbrown-menu ${show?"nilsbrown-menu-show": ""}`}id="nilsbrown-navbar-spy">
     <div className="nilsbrown-menu-inner" id="nilsbrown-navbar">
       <ul className="list-unstyled">
-        <li>
+        <li className={`${show?"is-show":""}`}>
         <Link href="/">
         <a >Home</a>
         </Link>
         </li>
-        <li>
+        <li className={`${show?"is-show":""}`}>
           <Link href="/about">
           <a >Resume</a>
           </Link>
          
         </li>
-        <li>
+        <li className={`${show?"is-show":""}`}>
           <Link href="/services">
           <a >Services</a>
           </Link> 
         </li>
-        <li>
+        <li className={`${show?"is-show":""}`}>
           <Link href="/portfolio">
           <a >Portfolio</a>
           </Link>
           
         </li>
-        <li>
+        <li className={`${show?"is-show":""}`}>
           <Link href="/blog">
           <a >Blog</a>
           </Link> 
@@ -45,7 +156,7 @@ const Navbar = () => {
               </ul>
           </li>
           */}
-        <li>
+        <li className={`${show?"is-show":""}`}>
           <Link href="/contact">
           <a >Contact</a>
           </Link>
@@ -76,7 +187,7 @@ const Navbar = () => {
         {/* Menu Burger */}
         <div className="col-4 col-md-4 text-right nilsbrown-menu-burger-wrap">
           {" "}
-          <a href="#" className="nilsbrown-nav-toggle nilsbrown-js-nav-toggle">
+          <a href="#" className={`nilsbrown-nav-toggle nilsbrown-js-nav-toggle ${show?" active": ""}`} onClick={handleShow}>
             <i />
           </a>{" "}
         </div>
--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,27 @@ import '../styles/globals.css'
 import Script from 'next/script'
 import Head from 'next/head'
 import Layout from '../layout/layout'
+import { Router } from 'next/router';
+import { useEffect } from 'react';
+import dynamic from 'next/dynamic';
+var $ = require('jquery');
+if (typeof window !== 'undefined') {
+   window.$ = window.jQuery = require('jquery');
+}
+import 'owl.carousel/dist/assets/owl.carousel.css';
+
+
+if(typeof window !== "undefined"){
+
+
+}
 
 function MyApp({ Component, pageProps }) {
+
+   useEffect(() => {
+
+  }, [Router]);
+
   return(
     <>
     <Head>
@@ -16,16 +35,16 @@ function MyApp({ Component, pageProps }) {
     <Layout>
     <Component {...pageProps} />
     </Layout>
-       
-      <Script src="js/plugins/jquery-3.6.0.min.js" strategy="lazyOnload"/>
-      <Script src="js/plugins/bootstrap.min.js"/>
-      <Script src="js/plugins/owl.carousel.min.js"/>
-      <Script src="js/plugins/jquery.magnific-popup.min.js"/>
-      <Script src="js/plugins/YouTubePopUp.js"/>
-      <Script src="js/plugins/jquery.easing.1.3.js"/>
-      <Script src="js/plugins/smooth-scroll.min.js"/>
-      <Script src="js/plugins/wow.js"/>
-      <Script src="js/custom.js"/>
+
+    <Script src="js/plugins/jquery-3.6.0.min.js" />
+    <Script src="js/plugins/bootstrap.min.js" />
+    <Script src="js/plugins/owl.carousel.min.js" />
+    <Script src="js/plugins/jquery.magnific-popup.min.js" />
+    <Script src="js/plugins/YouTubePopUp.js" />
+    <Script src="js/plugins/jquery.easing.1.3.js" />
+    <Script src="js/plugins/smooth-scroll.min.js" />
+    <Script src="js/plugins/wow.js" />
+    {/* <Script src="js/custom.js" strategy="afterInteractive"/> */}
       </>
   )
   
